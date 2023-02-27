codecat/tm-dashboard

commit 197f9f7656f4ebf341c59753b4a2be080172850a

Added option for gearbox text to change color based on gear. (#65)

Source/Things/Gearbox.as
@@ -61,8 +61,28 @@ class DashboardGearbox : DashboardThing
 		nvg::Fill();
 		nvg::Stroke();
 
+		vec4 textColor = Setting_Gearbox_TextColor;
+		if (Setting_Gearbox_UseGearColors) {
+
+			switch (gear) {
+				case 0: textColor = Setting_Gearbox_Gear0Color; break;
+				case 1: textColor = Setting_Gearbox_Gear1Color; break;
+				case 2: textColor = Setting_Gearbox_Gear2Color; break;
+				case 3: textColor = Setting_Gearbox_Gear3Color; break;
+				case 4: textColor = Setting_Gearbox_Gear4Color; break;
+				case 5: textColor = Setting_Gearbox_Gear5Color; break;
+				#if TM4 || TURBO
+					case 6: textColor = Setting_Gearbox_Gear6Color; break;
+					case 7: textColor = Setting_Gearbox_Gear7Color; break;
+				#endif
+				#if TURBO
+					case 8: textColor = Setting_Gearbox_Gear8Color; break;
+					case 9: textColor = Setting_Gearbox_Gear9Color; break;
+				#endif
+			}
+		}
 		nvg::FontFace(m_font);
-		nvg::FillColor(Setting_Gearbox_TextColor);
+		nvg::FillColor(textColor);
 		nvg::TextAlign(nvg::Align::Middle | nvg::Align::Center);
 
 		float gearY = 0.37f;

Source/Settings.as
@@ -200,6 +200,42 @@ vec4 Setting_Gearbox_TextColor = vec4(1, 1, 1, 1);
 [Setting category="Gearbox" name="Font"]
 string Setting_Gearbox_Font = "DroidSans.ttf";
 
+[Setting category="Gearbox" name="Use gear colors"]
+bool Setting_Gearbox_UseGearColors = false;
+
+[Setting category="Gearbox" name="Gear 0 (backwards) color" color]
+vec4 Setting_Gearbox_Gear0Color = vec4(1, 1, 1, 1);
+
+[Setting category="Gearbox" name="Gear 1 color" color]
+vec4 Setting_Gearbox_Gear1Color = vec4(1, 1, 1, 1);
+
+[Setting category="Gearbox" name="Gear 2 color" color]
+vec4 Setting_Gearbox_Gear2Color = vec4(1, 1, 1, 1);
+
+[Setting category="Gearbox" name="Gear 3 color" color]
+vec4 Setting_Gearbox_Gear3Color = vec4(1, 1, 1, 1);
+
+[Setting category="Gearbox" name="Gear 4 color" color]
+vec4 Setting_Gearbox_Gear4Color = vec4(1, 1, 1, 1);
+
+[Setting category="Gearbox" name="Gear 5 color" color]
+vec4 Setting_Gearbox_Gear5Color = vec4(1, 1, 1, 1);
+
+#if MP4 || TURBO
+	[Setting category="Gearbox" name="Gear 6 color" color]
+	vec4 Setting_Gearbox_Gear6Color = vec4(1, 1, 1, 1);
+
+	[Setting category="Gearbox" name="Gear 7 color" color]
+	vec4 Setting_Gearbox_Gear7Color = vec4(1, 1, 1, 1);
+#endif
+
+#if TURBO
+	[Setting category="Gearbox" name="Gear 8 color" color]
+	vec4 Setting_Gearbox_Gear8Color = vec4(1, 1, 1, 1);
+
+	[Setting category="Gearbox" name="Gear 9 color" color]
+	vec4 Setting_Gearbox_Gear9Color = vec4(1, 1, 1, 1);
+#endif
 
 enum AccelerationUnit
 {

commit 2507574011eca717cb22f32f990dcbf96d8d2e80

Fixed broken preprocessor directives introduced in #65

Source/Things/Gearbox.as
@@ -78,14 +78,14 @@ class DashboardGearbox : DashboardThing
 				case 3: textColor = Setting_Gearbox_Gear3Color; break;
 				case 4: textColor = Setting_Gearbox_Gear4Color; break;
 				case 5: textColor = Setting_Gearbox_Gear5Color; break;
-				#if TM4 || TURBO
-					case 6: textColor = Setting_Gearbox_Gear6Color; break;
-					case 7: textColor = Setting_Gearbox_Gear7Color; break;
-				#endif
-				#if TURBO
-					case 8: textColor = Setting_Gearbox_Gear8Color; break;
-					case 9: textColor = Setting_Gearbox_Gear9Color; break;
-				#endif
+#if MP4 || TURBO
+				case 6: textColor = Setting_Gearbox_Gear6Color; break;
+				case 7: textColor = Setting_Gearbox_Gear7Color; break;
+#endif
+#if TURBO
+				case 8: textColor = Setting_Gearbox_Gear8Color; break;
+				case 9: textColor = Setting_Gearbox_Gear9Color; break;
+#endif
 			}
 		}
 		nvg::FontFace(m_font);